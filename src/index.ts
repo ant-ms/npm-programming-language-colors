fb2263b4a66f1156e4b2128e0898fb5aaac36f0b
Yanik Ammann <yanik.ammann@siemens.com>
2026-02-27 16:54:54 +0100

    chore: do not bundle json as types

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ import LanguageData from "./languages.json" with { type: "json" };
 
 const { _disclaimer, ...LanguageColors } = LanguageData;
 
-const colors: Record<string, string> = LanguageColors;
+export const Colors: Record<string, string> = LanguageColors;
 
-const getLanguageColor = (language: string): string | undefined => {
-  return colors[language.toLowerCase()];
+export const getLanguageColor = (language: string): string | undefined => {
+  return Colors[language.toLowerCase()];
 };
-
-export { getLanguageColor, LanguageColors };
